3bd7483bcf491d02a7630aeb3485441b53bf63d8
Olga Smirnova <smrnva.olga@gmail.com>
2023-05-08 14:59:53 +0300

    W2D2_ExXP_changed

--- Week_2/Day_2/ExerciseXP/Ex_8.py
+++ Week_2/Day_2/ExerciseXP/Ex_8.py
@@ -1,5 +1,10 @@
 # 1. Write a loop that asks a user to enter a series of pizza toppings, 
 # when the user inputs ‘quit’ stop asking for toppings.
-# input = ' '
-# while input != 'quit' : 
-#     topping =
+
+# topping = ' '
+# toppings = [ ]
+# while topping != 'quit' : 
+# 	topping = input('What topping do you want to add? (Write \'quit\' when you want to stop) \n')
+# 	toppings = toppings.append(topping)
+
+# print(topping)
