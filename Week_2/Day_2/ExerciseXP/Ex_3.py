3c0107c0e9b41edce3100b5e3bd66a94a39e5b84
Olga Smirnova <smrnva.olga@gmail.com>
2023-05-08 14:02:49 +0300

    W2D2_ExXP_Ex3_changed

--- Week_2/Day_2/ExerciseXP/Ex_3.py
+++ Week_2/Day_2/ExerciseXP/Ex_3.py
@@ -13,11 +13,13 @@ basket.append('Kiwi')
 basket.insert(0, 'Apples')
 
 # 5. Count how many apples are in the basket.
-count = 0
 
-for fruit in basket : 
-	if fruit == 'Apples' :
-		count +=1
+# count = 0
+# for fruit in basket : 
+# 	if fruit == 'Apples' :
+# 		count +=1
+
+count = basket.count('Apples')
 
 # 6. Empty the basket.
 basket.clear()
---
+++ Week_2/Day_2/ExerciseXP/Ex_8.py
@@ -0,0 +1,5 @@
+# 1. Write a loop that asks a user to enter a series of pizza toppings, 
+# when the user inputs ‘quit’ stop asking for toppings.
+# input = ' '
+# while input != 'quit' : 
+#     topping =
