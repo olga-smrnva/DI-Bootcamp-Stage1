9c23f4c32b861940e34edfc7d1fbb8186cfbb2e4
Olga Smirnova <smrnva.olga@gmail.com>
2023-05-08 10:25:19 +0300

    W2D2_ExXP_Ex3

---
+++ Week_2/Day_2/ExerciseXP/Ex_3.py
@@ -0,0 +1,26 @@
+basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+
+# 1. Remove “Banana” from the list.
+basket.remove('Banana')
+
+# 2. Remove “Blueberries” from the list.
+basket.pop()
+
+# 3. Add “Kiwi” to the end of the list.
+basket.append('Kiwi')
+
+# 4. Add “Apples” to the beginning of the list.
+basket.insert(0, 'Apples')
+
+# 5. Count how many apples are in the basket.
+count = 0
+
+for fruit in basket : 
+	if fruit == 'Apples' :
+		count +=1
+
+# 6. Empty the basket.
+basket.clear()
+
+# 7. Print(basket)
+print(basket)
