0560e50f748626a1f178d85ade8182538aa73608
Olga Smirnova <smrnva.olga@gmail.com>
2023-05-08 12:14:36 +0300

    W2D2_ExXP_Ex6

---
+++ Week_2/Day_2/ExerciseXP/Ex_6.py
@@ -0,0 +1,8 @@
+# Write a while loop that will continuously ask the user for their name, 
+# unless the input is equal to your name.
+
+my_name = 'Olga'
+user_name = ' '
+
+while user_name != my_name :
+	user_name = input('Tell me your name \n')
