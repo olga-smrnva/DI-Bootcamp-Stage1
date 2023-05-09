26bf71dfefb8f08afb934fd82d0e03576039d6a7
Olga Smirnova <smrnva.olga@gmail.com>
2023-05-09 16:49:01 +0300

    W2D3_ExXP_Ex1

---
+++ Week_2/Day_3/ExerciseXP/Ex_1.py
@@ -0,0 +1,9 @@
+# Convert the two following lists, into dictionaries.
+# Hint: Use the zip method
+
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+dictionaries = dict(zip(keys, values))
+
+print(dictionaries)
