8f26e19fae394b2a8c0041fe52868598735b120b
Olga Smirnova <smrnva.olga@gmail.com>
2023-05-09 19:46:23 +0300

    W2D3_ExXP_4

--- Week_2/Day_3/ExerciseXP/EX_4.py
+++ Week_2/Day_3/ExerciseXP/EX_4.py
@@ -20,15 +20,40 @@ for user in users:
     disney_users_B[number] = user
     number += 1
     
-# numbers = []
-# for number in range(0, 5) :
-#     numbers.append(number)
-
 print(disney_users_B)
 
 # 3. Use a method to recreate the 3rd result. Hint: The 3rd result is sorted alphabetically.
 
+sorted_users = users[:]
+sorted_users.sort()
+
+number = 0
+disney_users_C = {}
+
+for user in sorted_users:
+    disney_users_C[user] = number
+    number += 1
+
+print(disney_users_C)
 
 # 4. Only recreate the 1st result for:
 #	*The characters, which names contain the letter “i”.
+
+disney_users_A_copy1 = disney_users_A.copy()
+
+for user in users :
+    if 'i' not in user : 
+        del disney_users_A_copy1[user]
+        
+print(disney_users_A_copy1)
+
 #	*The characters, which names start with the letter “m” or “p”.
+
+disney_users_A_copy2 = disney_users_A.copy()
+
+for user in users:
+    if user.startswith('P') or user.startswith('M') :
+        del disney_users_A_copy2[user]
+        
+print(disney_users_A_copy2)
+
