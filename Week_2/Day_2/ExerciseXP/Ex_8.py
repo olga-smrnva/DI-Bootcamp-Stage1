2dfe9b20f123953a37d2c5f83d564836a8dad32d
Olga Smirnova <smrnva.olga@gmail.com>
2023-05-09 10:26:12 +0300

    W2D2_ExXP_Ex8

--- Week_2/Day_2/ExerciseXP/Ex_8.py
+++ Week_2/Day_2/ExerciseXP/Ex_8.py
@@ -1,10 +1,20 @@
 # 1. Write a loop that asks a user to enter a series of pizza toppings, 
 # when the user inputs ‘quit’ stop asking for toppings.
 
-# topping = ' '
-# toppings = [ ]
-# while topping != 'quit' : 
-# 	topping = input('What topping do you want to add? (Write \'quit\' when you want to stop) \n')
-# 	toppings = toppings.append(topping)
+# 2. As they enter each topping, print a message saying you’ll add that topping to their pizza.
 
-# print(topping)
+topping = input('What topping do you want to add? (Write \'quit\' when you want to stop) \n')
+toppings = []
+
+while topping != 'quit' : 
+	print(f'{topping} will be added to your pizza')
+	toppings.append(topping)
+	topping = input('What topping do you want to add? (Write \'quit\' when you want to stop) \n')
+
+# print(toppings)
+
+# 3. Upon exiting the loop print all the toppings on the pizza pie and what the total price is (10 + 2.5 for each topping).
+
+total = 10 + len(toppings) * 2.5
+
+print(f'Total cost will be {total}')
