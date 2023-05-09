8924f91ae860d088632d0ea742217fe8505fd34a
Olga Smirnova <smrnva.olga@gmail.com>
2023-05-09 09:04:46 +0300

    W2D2_ExXP_Ex9

--- Week_2/Day_2/ExerciseXP/Ex_9.py
+++ Week_2/Day_2/ExerciseXP/Ex_9.py
@@ -4,5 +4,18 @@
 #	* if they are over the age of 12, the ticket is $15
 
 # 2. Ask a family the age of each person who wants a ticket.
+ages = input('Give me all the ages of of family memebers separated by coma (without space). Press ENTER when you\'re finished \n')
+
+ages_list = ages.split(',')
+
+cost = 0
+
+for age in ages_list :
+    if int(age) < 3 : cost += 0
+    elif int(age) < 12 : cost += 10
+    else : cost += 15
+
 
 # 3. Store the total cost of all the family’s tickets and print it out.
+
+print(cost)
