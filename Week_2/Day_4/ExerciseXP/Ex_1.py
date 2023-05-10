e3bd710119c3e5d9229c4f69a7d0e7fdaac7f30e
Olga Smirnova <smrnva.olga@gmail.com>
2023-05-10 11:55:13 +0300

    W2D4_ExXP_1

---
+++ Week_2/Day_4/ExerciseXP/Ex_1.py
@@ -0,0 +1,8 @@
+# 1. Write a function called display_message() that prints one sentence telling everyone what you are learning in this course.
+
+def display_message() :
+    print('Hey there, I\'m stuidying Python!')
+
+# 2. Call the function, and make sure the message displays correctly.
+
+display_message()
