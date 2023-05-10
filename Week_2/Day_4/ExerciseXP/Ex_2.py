06ed90653fb224b5958744cd5880192767deea5d
Olga Smirnova <smrnva.olga@gmail.com>
2023-05-10 11:59:14 +0300

    W2D4_ExXP_2

---
+++ Week_2/Day_4/ExerciseXP/Ex_2.py
@@ -0,0 +1,10 @@
+# 1. Write a function called favorite_book() that accepts one parameter called title.
+
+# 2. The function should print a message, such as "One of my favorite books is <title>". For example: “One of my favorite books is Alice in Wonderland”
+
+def favorite_book(title) :
+    print(f'One of my favorite books is {title}')
+
+# 3. Call the function, make sure to include a book title as an argument when calling the function.
+
+favorite_book('Harry Potter')
