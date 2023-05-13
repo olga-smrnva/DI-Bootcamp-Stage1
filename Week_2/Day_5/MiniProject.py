3502eb9dbb24deb02d7200dc3ca81187fe781bf5
Olga Smirnova <smrnva.olga@gmail.com>
2023-05-13 22:18:48 +0300

    W2D5_miniProject_func_final

--- Week_2/Day_5/MiniProject.py
+++ Week_2/Day_5/MiniProject.py
@@ -1,49 +1,71 @@
-# display_board() – To display the Tic Tac Toe board (GUI).
-# player_input(player) – To get the position from the player.
-# check_win() – To check whether there is a winner or not.
-# play() – The main function, which calls all the functions created above.
+def display_board(board):
+	print("*****************")
+	for row in board :
+		print(f"*   {row[0]} | {row[1]} | {row[2]}   *")
+		print("*  ---|---|---  *")
+	print("*****************")
 
-board = [
-    [" ", " ", " "],
-    [" ", " ", " "],
-    [" ", " ", " "]
-]
-player = 'X' # X or O
-
-
-
-def display_board():
-    print("*****************")
-    for row in board :
-        print(f"*   {row[0]} | {row[1]} | {row[2]}   *")
-        print("*  ---|---|---  *")
-    print("*****************")
-
-def player_input(player):
-    allowed_input = ["1", "2", "3"]
-    while True:
-        row = input("Enter row(1-3): ")
-        column  = input("Input column(1-3): ")
-        if row in allowed_input and column in allowed_input:
-            return {row: int(row), column: int(column)}
+def player_input(player, board):
+	allowed_input = ["1", "2", "3"]
+	while True:
+		row = input("Enter row(1-3): ")
+		column  = input("Input column(1-3): ")
+		if row in allowed_input and column in allowed_input:
+			board[int(row)-1][int(column)-1] = player
+			return 
 
 
-def check_win():
-    pass
+def check_win(player, board):
+	lines_to_check = []
+
+	# get all rows and columns to check
+	for index in range(3): 
+		row = board[index]
+		column = [board[0][index], board[1][index], board[2][index]]
+		lines_to_check.extend([row, column])
+	
+	# get both diagonals to check
+	diagonal1 = [board[0][0], board[1][1], board[2][2]]
+	diagonal2 = [board[0][2], board[1][1], board[2][0]]
+	lines_to_check.extend([diagonal1, diagonal2])
+
+	#check all rows, columns and diagonals for a winner
+	for line in lines_to_check:
+		if line[0] != " " and len(set(line)) == 1:
+			return player
+	
+	#check if the game ends in a tie
+	for row in board:
+		for cell in row:
+			if cell == " ":
+				return ""
+	return "tie"
+
 
 def play():
-    winner_player = ''
-    print("Welcome to TIC TAC TOE!")
+	board = [
+	[" ", " ", " "],
+	[" ", " ", " "],
+	[" ", " ", " "]
+]
+	current_player = 'X' # X or O
+	winner_player = ''
 
-    while not winner_player:
-        display_board()
-        print(f"Player {player}'s turn...")
-        player_input(player)
-        is_win = check_win()
-    print('The game ends...')
-    if winner_player == "tie":
-        print("in a tie :(")
-    else:
-        print(f"And {winner_player} wins!!!")
-        
+	print("Welcome to TIC TAC TOE!")
 
+	while not winner_player:
+		display_board(board)
+		print(f"Player {current_player}'s turn...")
+		player_input(current_player, board)
+		winner_player = check_win(current_player, board)
+		current_player = "O" if current_player == "X" else "X"
+
+	print('The game ends...')
+	display_board(board)
+	if winner_player == "tie":
+		print("in a tie :(")
+	else:
+		print(f"And {winner_player} wins!!!")
+		
+
+play()
