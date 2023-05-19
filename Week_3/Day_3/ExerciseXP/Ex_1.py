eaad4aaf4e51cf19f50583d3b31f35e5e0cf5867
Olga Smirnova <smrnva.olga@gmail.com>
2023-05-19 15:08:56 +0300

    Minor changes

---
+++ Week_3/Day_3/ExerciseXP/Ex_1.py
@@ -0,0 +1,4 @@
+# 1. Write a program which prints the results of the following python built-in functions: abs(), int(), input().
+
+
+# 2. Using the __doc__ dunder method create your own documentation which explains the execution of your code. Take a look at the doc method on google for help.
