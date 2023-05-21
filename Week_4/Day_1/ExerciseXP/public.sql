0b1b780aa6740b2aca26c20207b3b542c55e7379
Olga Smirnova <smrnva.olga@gmail.com>
2023-05-21 18:37:38 +0300

    W4D1_exXP

---
+++ Week_4/Day_1/ExerciseXP/public.sql
@@ -0,0 +1,9 @@
+-- SELECT * FROM CUSTOMERS
+-- SELECT * FROM ITEMS
+-- SELECT * FROM ITEMS WHERE item_price > 80
+-- SELECT * FROM ITEMS WHERE item_price <= 300
+-- SELECT * FROM CUSTOMERS where first_name = 'Smith'
+-- SELECT * FROM CUSTOMERS where second_name = 'Jones'
+-- SELECT * FROM CUSTOMERS where first_name != 'Scott'
+
+
