731845d397d8b28d4670c27c5c849a60edfb99aa
Olga Smirnova <smrnva.olga@gmail.com>
2023-05-22 10:05:34 +0300

    W4D1_exXP

--- Week_4/Day_1/ExerciseXP/public.sql
+++ Week_4/Day_1/ExerciseXP/public.sql
@@ -1,3 +1,25 @@
+-- CREATE TABLE items (
+-- 	item_id SERIAL PRIMARY KEY,
+-- 	item_name VARCHAR(100),
+-- 	price SMALLINT 
+-- )
+
+-- CREATE TABLE customers (
+-- 	customer_id SERIAL PRIMARY KEY,
+-- 	first_name VARCHAR(100),
+-- 	last_name VARCHAR(100)
+-- )
+
+-- INSERT INTO items (item_name, price)
+-- VALUES ('Small Desk', 100), ('Large Desk', 300), ('Fan', 80)
+
+-- INSERT INTO customers (first_name, last_name)
+-- VALUES ('Greg', 'Jones'),
+-- ('Sandra', 'Jones'),
+-- ('Scott', 'Scott'),
+-- ('Trevor', 'Green'),
+-- ('Melanie', 'Johnson');
+
 -- SELECT * FROM CUSTOMERS
 -- SELECT * FROM ITEMS
 -- SELECT * FROM ITEMS WHERE item_price > 80
